fafb36adda31d0b89e819871d4ffdc37d21f22fc
Uzair Minhas <uziminhas@gmail.com>
2020-04-25 21:48:36 -0400

    Update for cron works and redis

--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,17 @@
 /*
 We will put our function here to fetch jobs
-
-
 */
-
-
 var CronJob = require('cron').CronJob;
+
+
+const fetchGitHub = require('./tasks/fetch-github.js');
+
+// Fetch github jobs
+new CronJob('* * * * *', fetchGitHub, null, true, 'America/Los_Angeles');
+
+/*
 var job = new CronJob('* * * * * *', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
 job.start();
+*/
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -4,6 +4,13 @@ Backend fetch of Githubs API
 */
 
 var fetch = require('node-fetch');
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+//const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
 
 // Add a query string for each page until you get to the end
 
@@ -11,21 +18,30 @@ const baseURL = 'https://jobs.github.com/positions.json'
 
 async function fetchGitHub() {
 
+	console.log('fetching github');
+
 	// variable to track resultCount and onPage (i.e. what page we are on)
 	let resultCount = 1, onPage = 0;
 
 	// array to hold jobs
-	const 
+	const allJobs = [];
 
 	while(resultCount > 0) {
 		// run this subroutine for every page that is not empty 
-		const res = await fetch('${baseURL}?page=${onPage}'); //create a template literal
-		const jobs = await res.json();
-		console.log({jobs});
-		console.log(jobs.length);	
+		const res = await fetch(`${baseURL}?page=${onPage}`); //create a template literal
+		const jobs = await res.json(); //Json from the response
+		allJobs.push(...jobs); // Putting JSON into an array, spread operator
+		resultCount = jobs.length;
+		console.log('got', resultCount, 'jobs');
+		onPage++;
+
 	}
+  
+	console.log	('got', allJobs.length, 'jobs total');
+	const success = await setAsync('github', JSON.stringify(allJobs));
+
+	console.log({success});
 	
 }
 
-
-module.exports = fetchGitHub();
+module.exports = fetchGitHub;
