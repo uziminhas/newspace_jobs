eb2d25afe3d4d3884104ce1fa53fb55636bbd896
Uzair Minhas <uziminhas@gmail.com>
2020-08-23 22:38:52 -0400

    Change form field

--- client/src/TalentMatcher.js
+++ client/src/TalentMatcher.js
@@ -358,7 +358,7 @@ export default function SignUp() {
                         </MenuItem>
                       ))}
                     </Select>
-                    {state.roleError && <FormHelperText>Please select an occupation / job</FormHelperText>}
+                    {state.roleError && <FormHelperText>Please select an occupation</FormHelperText>}
                   </FormControl>
                 </Grid>
                 <Grid item xs={12}>
